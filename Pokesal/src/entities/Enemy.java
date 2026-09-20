f449350949d75f6055e2d149a8365c4b77daf495
Anielly Nunes <aniellynunes178@gmail.com>
2026-09-20 01:15:37 -0300

    Format Enemy class and improve code readability
    Falta o javadoc

--- Pokesal/src/entities/Enemy.java
+++ Pokesal/src/entities/Enemy.java
@@ -2,17 +2,17 @@ package entities;
 
 public class Enemy extends Trainer {
 
-	public Enemy(String nome, Pokesal pokesal) {
-		super(nome);
-		this.choosePokesal(pokesal);
-	}
-	
-	public int action() {
-		//usar item
-		if(this.getChoosedPokesal().getHp()<10 && !this.getBag().isEmpty()) {
-			return 2;
-		}
-		//ataca
-		return 1;
-	}
+  public Enemy(String nome, Pokesal pokesal) {
+    super(nome);
+    this.choosePokesal(pokesal);
+  }
+
+  public int action() {
+    // usar item
+    if (this.getChoosedPokesal().getHp() < 10 && !this.getBag().isEmpty()) {
+      return 2;
+    }
+    // ataca
+    return 1;
+  }
 }
