595f5af6175f0ec1799d5b7e1f08266cb2a31bb4
Anielly Nunes <aniellynunes178@gmail.com>
2026-09-20 23:02:09 -0300

    Add Javadoc comments to Campo class
    Added documentation comments to the Campo class and its methods.

--- Pokesal/src/entities/Campo.java
+++ Pokesal/src/entities/Campo.java
@@ -2,15 +2,31 @@ package entities;
 
 import java.util.Random;
 
+/**
+ * Representa o campo/cenário onde a batalha acontece.
+ * O cenário é gerado aleatoriamente e gera vantagens ou
+ * recuperação de vida dependendo do tipo do pokesal.
+ *
+ */
 public class Campo {
   private String cenario;
 
+  /**
+   * Instância o Campo, sorteando aleatoriamente um cenário,
+   * entre as opções disponíveis.
+   */
   public Campo() {
     String[] cenarios = { "asfalto quente", "poca de chuva", "piso molhado", "canteiro central" };
     Random r = new Random();
     this.cenario = cenarios[r.nextInt(cenarios.length)];
   }
 
+  /**
+   * Avalia a vantagem de campo para um determinado Pokesal com base em seu tipo elemental.
+   *
+   * @param p recebe o benefício do campo.
+   * @return O multiplicador de vantagem dado pelo campo.
+   */
   public double vantagemCampo(Pokesal p) {
     if (cenario.equals("asfalto quente") && p.getTipo().equals("fogo")) {
       return 1.15;
