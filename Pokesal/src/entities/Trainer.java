d5f760d4998b90d231cc6331d08e40fec62c1630
Anielly Nunes <aniellynunes178@gmail.com>
2026-09-20 22:38:29 -0300

    Add JavaDoc comments to Trainer class
    Added JavaDoc comments to the Trainer class and its methods.

--- Pokesal/src/entities/Trainer.java
+++ Pokesal/src/entities/Trainer.java
@@ -3,20 +3,31 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Representa um treinador no jogo, responsável por seu Pokesal escolhido.
+ *
+ */
 public class Trainer {
   private String nome;
   private Pokesal choosedPokesal;
   private List<Item> bag;
 
+  /**
+   * Nome do treinador.
+   */
   public Trainer(String nome) {
     this.nome = nome;
     this.bag = new ArrayList<>();
   }
 
+  /**
+   * O Pokesal atualmente selecionado pelo treinador.
+   */
   public void choosePokesal(Pokesal pokesal) {
     this.choosedPokesal = pokesal;
   }
 
+  /** Lista de itens contidos na mochila do treinador. */
   public void buyItem(Item item) {
     if (bag.size() < 2) {
       bag.add(item);
