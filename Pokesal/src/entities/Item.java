3333a3862e5b97c8baa68bebb3350db8fd32405b
Anielly Nunes <aniellynunes178@gmail.com>
2026-09-20 22:45:56 -0300

    Refactor Item class and add documentation
    Refactored the Item class to remove duplicate code and added documentation.

--- Pokesal/src/entities/Item.java
+++ Pokesal/src/entities/Item.java
@@ -1,18 +1,14 @@
 package entities;
 
-public class Item {
-	
-	private String nomeItem;
-
-	public Item(String nomeItem) {
-		super();
-		this.nomeItem = nomeItem;
-		package entities;
-
+/**
+ * Representa um item utilizável no jogo que pode aplicar efeitos e alterações de status.
+ *
+ */
 public class Item {
 
   private String nomeItem;
 
+  /** Nome do item. */
   public Item(String nomeItem) {
     super();
     this.nomeItem = nomeItem;
@@ -22,6 +18,11 @@ public class Item {
     return nomeItem;
   }
 
+  /**
+   * Aplica o efeito do item fornecido com base no nome do item.
+   * Possui os efeitos possíveis para uso.
+   *
+   */
   public void efeitoItem(Pokesal pokesal) {
     if (this.nomeItem.equalsIgnoreCase("potion")) {
       double newHp = pokesal.getHp() + 20;
@@ -47,38 +48,4 @@ public class Item {
       }
     }
   }
-}
-
-	}
-
-	public String getNomeItem() {
-		return nomeItem;
-		
-	}
-
-	public void efeitoItem(Pokesal pokesal) {
-        if (this.nomeItem.equalsIgnoreCase("potion")) {
-            double newHp = pokesal.getHp() + 20;
-            if (newHp>pokesal.getMaxHp()) newHp=pokesal.getMaxHp();
-            pokesal.setHp(newHp);
-            System.out.println(pokesal.getNome() + " recuperou 20 de HP!");            
-        
-        } else if (this.nomeItem.equals("x-atk")) {
-            int newAtk = (int) (pokesal.getAtk() * 1.15);
-            pokesal.setAtk(newAtk); 
-            System.out.println("O Ataque de "+ pokesal.getNome() + " subiu em 15%!");
-            
-        } else if (this.nomeItem.equals("x-def")) {
-            int newDef = (int) (pokesal.getDef() * 1.15);
-            pokesal.setDef(newDef); 
-            System.out.println("A Defesa de " + pokesal.getNome() + " subiu em 15%!");
-        
-        } else if (this.nomeItem.equals("x-spd")) {
-            int newSpd = (int) (pokesal.getSpd() * 1.15);
-            pokesal.setSpd(newSpd); 
-            System.out.println("Velocidade de " + pokesal.getNome() + " subiu em 15%!");
-            
-        } 
-    }
-	
 }
