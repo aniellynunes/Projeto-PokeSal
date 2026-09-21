ba4dab5c655e2abe8d9ecb6c723963614b1b9c9d
Anielly Nunes <aniellynunes178@gmail.com>
2026-09-20 22:52:56 -0300

    Enhance Enemy class with JavaDoc comments
    Added documentation comments to the Enemy class and its methods.

--- Pokesal/src/entities/Enemy.java
+++ Pokesal/src/entities/Enemy.java
@@ -1,12 +1,28 @@
 package entities;
 
+/**
+ * Representa um treinador oponente (inimigo) no jogo, que herda as características
+ * Possui lógica própria de tomada de decisão.
+ *
+ */
 public class Enemy extends Trainer {
 
+  /**
+   * Constrói um novo oponente com um nome e um Pokesal inicial já atribuído.
+   *
+   * @param nome O nome do oponente.
+   * @param pokesal, o Pokesal que o oponente utilizará em batalha.
+   */
   public Enemy(String nome, Pokesal pokesal) {
     super(nome);
     this.choosePokesal(pokesal);
   }
 
+  /**
+   * Determina a ação a ser realizada pelo oponente no turno atual com base em sua inteligência.
+   *
+   * @return Um valor inteiro representando a ação escolhida, ataque ou item.
+   */
   public int action() {
     // usar item
     if (this.getChoosedPokesal().getHp() < 10 && !this.getBag().isEmpty()) {
